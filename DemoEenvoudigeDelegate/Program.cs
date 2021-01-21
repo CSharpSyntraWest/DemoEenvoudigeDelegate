5497b07fbe31b072da55fa8d3843b7025507446d
Helena Copppieters <hcoppieters@hotmail.com>
2021-01-21 09:43:03 +0100

    Add project files.

---
+++ DemoEenvoudigeDelegate/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DemoEenvoudigeDelegate
+{
+    public delegate void EenvoudigeDelegate(string param);
+    class Program
+    {
+        static void TestMethode(string text)
+        {
+            Console.WriteLine($"TestMethode is aangeroepen met param {text}");
+        }
+        static void TestMethode2(string mijnTekst)
+        {
+            Console.WriteLine($"TestMethode is (ook) aangeroepen met param {mijnTekst}");
+        }
+        static void Main(string[] args)
+        {
+            //Aanroep op klassieke manier (zonder delegate):
+            TestMethode("'Hello!'");
+            //Aanroep van methode via EenvoudigeDelegate
+            EenvoudigeDelegate mijnMethodVariabele = new EenvoudigeDelegate(TestMethode);
+            mijnMethodVariabele("'Nog Eens Hello via Delegate'");
+            EenvoudigeDelegate mijnMethodeVariabele2 = new EenvoudigeDelegate(TestMethode2);
+            mijnMethodeVariabele2("'parameter voor TestMethode2'");
+        }
+    }
+}
